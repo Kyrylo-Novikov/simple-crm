895dee7dd9a3e37d41473bb5adfdd5e00ea70a68
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-02 15:30:33 +0200

    feat(app.config): import enviroment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { firebaseConfig } from './environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { firebaseConfig } from './environments/enviroment';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [
