a8ece3eb66ebf6c98833c4a8b05b17234a7a16a1
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-02 15:29:54 +0200

    feat(app.routs): add routes for dashboard and user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './components/dashboard/dashboard';
+import { User } from './components/user/user';
+
+export const routes: Routes = [
+  { path: '', component: Dashboard },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'user', component: User },
+];
