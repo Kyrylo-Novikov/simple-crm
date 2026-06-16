19db03ff570ea41e094ec474222063c2b0f0a23b
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-16 15:04:26 +0200

    refactor(dialog-add-user): clean up component and apply styling

--- src/app/components/dialog-add-user/dialog-add-user.ts
+++ src/app/components/dialog-add-user/dialog-add-user.ts
@@ -10,19 +10,16 @@ import {
   MatDialogContent,
   MatDialogRef,
   MatDialogTitle,
+  MatDialogModule,
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { UserIF } from '../../models/user-interface.class';
-import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
-import { Firestore, collection, collectionData, addDoc, query } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-add-user',
-  providers: [provideNativeDateAdapter()],
   imports: [
     MatProgressBarModule,
     MatDatepickerModule,
@@ -39,21 +36,27 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 })
 export class DialogAddUser {
   loading = false;
-  user: UserIF = { firstName: '', lastName: '', birthDate: 0, street: '', zipCode: 0, city: '' };
+  user: UserIF = {
+    firstName: '',
+    lastName: '',
+    birthDate: 0,
+    email: '',
+    street: '',
+    zipCode: 0,
+    city: '',
+  };
   birthDate!: Date;
   readonly dialogRef = inject(MatDialogRef<DialogAddUser>);
   firestore: Firestore = inject(Firestore);
-  onCalcelClick() {
+  onCancelClick() {
     this.dialogRef.close();
   }
 
   async saveUser() {
     this.loading = true;
     this.user.birthDate = this.birthDate.getTime();
-    console.log('current user is :', this.user);
     const userCollection = collection(this.firestore, 'users');
     await addDoc(userCollection, this.user);
-    console.log('User erfolgreich gespeichert!');
-    this.onCalcelClick();
+    this.onCancelClick();
   }
 }
