e1b68ef12ce85dc73489280bbf303fe5e340d765
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-16 14:59:03 +0200

    feat(routes): add user-detail route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { Dashboard } from './components/dashboard/dashboard';
 import { User } from './components/user/user';
+import { UserDetail } from './components/user-detail/user-detail';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
   { path: 'dashboard', component: Dashboard },
   { path: 'user', component: User },
+  { path: 'user/:id', component: UserDetail },
 ];
