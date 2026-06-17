e59e890e530657a1b19bbbc11f1c47239f9c5b4e
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-17 06:37:58 +0200

    feat(dialog-edit-user): implement component

---
+++ src/app/components/dialog-edit-user/dialog-edit-user.ts
@@ -0,0 +1,82 @@
+import { ChangeDetectionStrategy, Component, inject, model, signal, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { UserIF } from '../../models/user-interface.class';
+import { Firestore, collection, addDoc, doc, updateDoc } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+// import { provideNativeDateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-edit-user',
+  imports: [
+    MatDialogActions,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatDialogContent,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './dialog-edit-user.html',
+  styleUrl: './dialog-edit-user.scss',
+})
+export class DialogEditUser implements OnInit {
+  firestore = inject(Firestore);
+  user!: UserIF;
+  birthDate!: Date;
+  readonly dialogRef = inject(MatDialogRef<DialogEditUser>);
+  private receivedUserData = inject(MAT_DIALOG_DATA);
+  loading = false;
+
+  onCancelClick() {
+    this.dialogRef.close();
+  }
+
+  async saveEdit() {
+    this.loading = true;
+    this.user.birthDate = this.birthDate.getTime();
+    const userDoc = doc(this.firestore, `users/${this.user.id}`);
+    const updateUser = {
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      birthDate: this.user.birthDate,
+      email: this.user.email,
+    };
+    try {
+      await updateDoc(userDoc, updateUser);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.loading = false;
+      this.onCancelClick();
+      console.log(updateUser);
+
+      console.log(this.user);
+    }
+  }
+
+  ngOnInit(): void {
+    this.user = this.receivedUserData;
+    console.log('die id :', this.user.id);
+
+    if (this.user.birthDate) {
+      this.birthDate = new Date(this.user.birthDate);
+    }
+    console.log(this.user);
+  }
+}
