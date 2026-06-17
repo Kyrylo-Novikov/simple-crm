1302f2f18710b5bf0c100d8cab993e7226cafea7
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-17 06:50:26 +0200

    refactor(user): clean up firestore query and remove manual subscription

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -10,9 +10,10 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
 import { UserIF } from '../../models/user-interface.class';
 import { MatCardModule } from '@angular/material/card';
-import { Firestore, collection, collectionData, addDoc, query } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Observable } from 'rxjs';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -27,6 +28,7 @@ import { Observable } from 'rxjs';
     ReactiveFormsModule,
     FormsModule,
     MatDialogModule,
+    RouterLink,
   ],
   templateUrl: './user.html',
   styleUrl: './user.scss',
@@ -34,8 +36,7 @@ import { Observable } from 'rxjs';
 export class User {
   firestore = inject(Firestore);
   private userCollection = collection(this.firestore, 'users');
-  userQuery = query(this.userCollection);
-  users$ = collectionData(this.userQuery) as Observable<UserIF[]>;
+  users$ = collectionData(this.userCollection, { idField: 'id' }) as Observable<UserIF[]>;
   users = toSignal(this.users$, { initialValue: [] });
   readonly dialog = inject(MatDialog);
 
@@ -45,10 +46,5 @@ export class User {
     });
   }
 
-  constructor() {
-    this.users$.subscribe({
-      next: (data) => console.log('DATA', data),
-      error: (err) => console.error('SUB ERROR', err),
-    });
-  }
+  constructor() {}
 }
